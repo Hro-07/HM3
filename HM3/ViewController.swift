6ebfc063babd9afbd9bff4c8515d2316ed782127
Hro-07 <hrostepanyan5@gmail.com>
2023-09-29 11:10:17 +0200

    Initial Commit

---
+++ HM3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM3
+//
+//  Created by Hrayr on 29/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
